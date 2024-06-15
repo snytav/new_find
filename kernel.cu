17d1234a1c39f94d121a03dccdba6eabb3649fd9
Alexey Snytnikov <snytav@gmail.com>
2024-06-15 23:44:55 +0200

    1st

---
+++ kernel.cu
@@ -0,0 +1,27 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+#include "cuPrintf.cuh"
+#include "cuPrintf.cu"
+
+__global__ void test()
+{
+      cuPrintf("test");
+}
+
+
+int main()
+{
+
+    cudaPrintfInit();
+    test << <1, 10 >> > ();
+    cudaPrintfDisplay(stdout, true);
+    cudaPrintfEnd();
+
+
+
+    return 0;
+}
