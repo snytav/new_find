1c499ff7c41687911f581966e898720695a654b2
Alexey Snytnikov <snytav@gmail.com>
2024-06-18 19:07:39 +0200

    making gap stage 0

--- kernel.cu
+++ kernel.cu
@@ -11,17 +11,22 @@
 #define SIZE_OF_LONG_INT 64
 
 //максимальная длина массива из длинных целых (для буферного массива я ядре find)
-#define N 2
+#define N 4
 __global__ void test()
 {
       cuPrintf("test");
 }
 
-unsigned int __device__ gap(int level)
+void  __global__ gap(int level)
 {
-    int denom = (int)pow(2, level);
-    unsigned int g = (gridDim.x * blockDim.x) / level / denom;
-    return g;
+    cuPrintf("gap entered level %d\n",level);
+    int d2 = (int)pow(2, level);
+    int g = (gridDim.x * blockDim.x) / d2 / level;
+   // cuPrintf("gap\n");
+    //cuPrintf("gap %d gridDim.x %d blockDim.x %d denom %d level %d\n",
+    //          gap,   (int)gridDim.x,   (int)blockDim.x,d2,level);
+
+    //return g;
 }
 
 unsigned int __device__ get_num_thread_to_compare(int level)
@@ -29,24 +34,27 @@ unsigned int __device__ get_num_thread_to_compare(int level)
    
     unsigned int n = blockIdx.x * blockDim.x + threadIdx.x;
 
-    return gap(level) + n;
+    //return gap(level) + n;
 }
 
 int __device__ active_thread(int level)
 {
     unsigned int n = blockIdx.x * blockDim.x + threadIdx.x;
-    return (n < gap(level));
+//    return (n < gap(level));
 }
 
 
 //считается что size равен количеству потоков
 void __global__ find(unsigned long long* d_v, int size, int* res)
 {
+
     __shared__ int res_by_thread[N];
     int local_1st_nonzero;
     unsigned int n = blockIdx.x * blockDim.x + threadIdx.x;
     local_1st_nonzero = __ffsll(d_v[n]);  // первая единица в слове
-    int levels = int(log((double)N) / log(2.0));
+    int levels = int(log((double)(N/2)) / log(2.0));
+
+   
    
     //номер этой первой единицы глобальный (по всему массиву) начиная справа
     res_by_thread[n] = local_1st_nonzero + SIZE_OF_LONG_INT * (size-n-1);
@@ -54,14 +62,16 @@ void __global__ find(unsigned long long* d_v, int size, int* res)
         local_1st_nonzero,res_by_thread[n]);
     
 
-    cuPrintf("reduction levels %d active %d gap %d \n",levels, active_thread(1),gap(1));
-   
-    for (int l = 1; l <= levels; l++)
+    //cuPrintf("reduction levels %d active %d gap %d \n",levels, active_thread(levels),gap(levels));
+    return;
+    for (int l = levels; l >= 1; l--)
     {
+        cuPrintf("l in loop %d activ %d\n",l, active_thread(l));
         if (active_thread(l))
         {
             unsigned int m = get_num_thread_to_compare(l);
-            cuPrintf("level %d gap %u compare with %u \n", l,gap(l), m);
+//            cuPrintf("level %d gap %u compare with %u res_by_thread[n] %d res_by_thread[m] %d \n",
+//                l,gap(l), m, res_by_thread[n], res_by_thread[m]);
             res_by_thread[n] = min(res_by_thread[n],
                                         res_by_thread[m]);
             cuPrintf("res_by_thread[n] %d res_by_thread[m] %d \n",
@@ -78,7 +88,8 @@ void __global__ find(unsigned long long* d_v, int size, int* res)
 
 int main()
 {
-    unsigned long long h_v[] = {0xABCDABCDABCD0000, 0x0F08000800080070};
+    unsigned long long h_v[] = {0xABCDABCDABCD0000, 0x0F08000800080070,
+                                0xABCDABCDAB100000, 0x0F08000800080700};
     unsigned long long* d_v;
     int *d_res,h_res;
 
@@ -86,7 +97,8 @@ int main()
     cudaMalloc(&d_res, sizeof(int));
     cudaMemcpy(d_v, h_v, N * sizeof(unsigned long long), cudaMemcpyHostToDevice);
     cudaPrintfInit();
-    find << <1, N >> > (d_v,N,d_res);
+    gap << <1, N >> > (4);
+    // find << <1, N >> > (d_v,N,d_res);
     cudaPrintfDisplay(stdout, true);
     cudaPrintfEnd();
     cudaMemcpy(&h_res, d_res, sizeof(int), cudaMemcpyDeviceToHost);
