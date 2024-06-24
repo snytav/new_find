0a4ec0e13b6642e55b244ad9770b0fa7d1ad6680
Snytnikova <snytnikovat@gmail.com>
2024-06-24 21:07:30 +0200

    проверены тестовые значения до 1024 потоков

--- kernel.cu
+++ kernel.cu
@@ -2,111 +2,98 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include "device_functions.h"
-
 #include <stdio.h>
 #include <time.h>
 #define MAX 999999
-
 #include "cuPrintf.cuh"
 #include "cuPrintf.cu"
-
 #define SIZE_OF_LONG_INT 64
-
-//максимальная длина массива из длинных целых (для буферного массива я ядре find  )
+//максимальная длина массива из длинных целых (для буферного массива я ядре find)
 #define N 1024
 __global__ void test()
 {
       cuPrintf("test");
 }
-
 int  __device__ gap(int level)
 {
     //cuPrintf("gap entered level %d\n",level);
-    int d2 = (int)pow(2, level);
+    int d2 = 1<<level;
     int g = (gridDim.x * blockDim.x) / d2;
     //cuPrintf("gap %d gridDim.x %d blockDim.x %d denom %d level %d\n",
       //        g,   (int)gridDim.x,   (int)blockDim.x,d2,level);
-
     return g;
 }
-
 unsigned int __device__ get_num_thread_to_compare(int level)
 {
    
     unsigned int n = blockIdx.x * blockDim.x + threadIdx.x;
-
     return gap(level) + n;
 }
-
 int __device__ active_thread(int level)
 {
     unsigned int n = blockIdx.x * blockDim.x + threadIdx.x;
     return (n < gap(level));
 }
-
-
 //считается что size равен количеству потоков
 void __global__ find(unsigned long long* d_v, int size, int* res)
 {
-
     __shared__ int res_by_thread[N];
     int local_1st_nonzero;
     unsigned int n = blockIdx.x * blockDim.x + threadIdx.x;
     local_1st_nonzero = __ffsll(d_v[n]);  // первая единица в слове
     int levels = int(log((double)(N)) / log(2.0));
-
    
    
     //номер этой первой единицы глобальный (по всему массиву) начиная справа
-    res_by_thread[n] = local_1st_nonzero + SIZE_OF_LONG_INT * (size-n-1);
-    cuPrintf("n %d res by thread local %d global %d\n",n, 
-        local_1st_nonzero,res_by_thread[n]);
+    res_by_thread[n] = ((local_1st_nonzero >0) && (local_1st_nonzero != 0xFFFFFFFF))*
+                                    (local_1st_nonzero + SIZE_OF_LONG_INT * n) +
+                       (local_1st_nonzero == 0)*(N*SIZE_OF_LONG_INT+1);
+    cuPrintf("n %d res by thread local %d global %d SIZE_OF_LONG_INT * n %d\n", 
+        n,
+        local_1st_nonzero,res_by_thread[n],
+        SIZE_OF_LONG_INT * n);
     
-
-    cuPrintf("reduction levels %d active %d gap %d \n",levels, active_thread(levels),gap(levels));
+//    cuPrintf("reduction levels %d active %d gap %d \n",levels, active_thread(levels),gap(levels));
     //return;
     for (int l = 1; l <= levels;l++)
     {
         cuPrintf("l in loop %d activ %d\n",l, active_thread(l));
+        __syncthreads();
         if (active_thread(l))
         {
             unsigned int m = get_num_thread_to_compare(l);
-            cuPrintf("level %d gap %u compare with %u res_by_thread[n] %d res_by_thread[m] %d \n",
-                l,gap(l), m, res_by_thread[n], res_by_thread[m]);
+            cuPrintf("level %d gap %u compare with %u res_by_thread[n=%d] %d res_by_thread[m=%d] %d \n",
+                l,gap(l), m,n, res_by_thread[n],m, res_by_thread[m]);
             res_by_thread[n] = min(res_by_thread[n],
                                         res_by_thread[m]);
             cuPrintf("res_by_thread[n] %d res_by_thread[m] %d \n",
                       res_by_thread[n],   res_by_thread[m]);
-
         }
     }
+    if (n==0)
+    {
+    	*res = res_by_thread[0];
+    // if (*res>length) *res = 0;
+    	if (*res>(N*SIZE_OF_LONG_INT)) *res = 0;
 
-   
-    *res = res_by_thread[n];
     cuPrintf("global min %d\n",*res);
+    }
 }
-
-
 int main()
 {
-    unsigned long long h_v[N];/* = {0x00000000000000001, 0x0F08000800080070,
+    unsigned long long h_v[N];/* = {0xABCDABCDABCD0000, 0x0F08000800080070,
                                 0xABCDABCDAB900000, 0x0F08000800080700,
                                 0xABCDABCDABC80000, 0x0F08000807000000,
                                 0xABCDABCDAB001000, 0x0F08000800080500 };*/
     unsigned long long* d_v;
     int *d_res,h_res;
-
     for (int i = 0; i < N; i++)
     {
-
-    	h_v[i]=(i==0)?1:0;
-/*        h_v[i] = rand() % MAX + 1;
+        h_v[i] = (i == (N-1)) ? 0x8000000000000000 : 0;//rand() % MAX + 1;
         int sh = rand() % 32 + 1;
-        h_v[i] <<= sh;
-        printf("%d %30lx shift %d \n",i,h_v[i],sh);
-        */
+        //h_v[i] <<= sh;
+        //printf("%d %30lx shift %d \n",i,h_v[i],sh);
     }
-
     cudaMalloc(&d_v, N * sizeof(unsigned long long));
     cudaMalloc(&d_res, sizeof(int));
     cudaMemcpy(d_v, h_v, N * sizeof(unsigned long long), cudaMemcpyHostToDevice);
@@ -117,7 +104,8 @@ int main()
     cudaPrintfEnd();
     cudaMemcpy(&h_res, d_res, sizeof(int), cudaMemcpyDeviceToHost);
 
-printf("FND=%i ", h_res);
+
+    printf("%d \n",h_res);
 
 
     return 0;
